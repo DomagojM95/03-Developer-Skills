a360c1c3fdd092beb489d095d5a09e827c5ab1f8
DomagojM95 <81382671+DomagojM95@users.noreply.github.com>
2021-05-10 11:20:59 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+const x = 45;
+
+console.log();
+
+const calcAge = (birthYear) => 2020 - birthYear;
+console.log(calcAge(1880));
