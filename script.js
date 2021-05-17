1ac47131cf2d2efe7fdc83c5af221e86b839b3a3
DomagojM95 <81382671+DomagojM95@users.noreply.github.com>
2021-05-17 22:43:48 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // Remember, we're gonna use strict mode in all scripts now!
+/*
 "use strict";
 
 const x = 45;
@@ -7,3 +8,25 @@ console.log();
 
 const calcAge = (birthYear) => 2020 - birthYear;
 console.log(calcAge(1880));
+
+*/
+
+const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
+
+const calcTempAmplitude = function (temps) {
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== "number") continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+
+const ammplitude = calcTempAmplitude(temperatures);
+console.log(ammplitude);
